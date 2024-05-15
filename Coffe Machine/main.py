650314cf8804496f4bc1c4de97e36a9f743068f1
장태영 <jangtaeyeong@jangtaeyeong-ui-MacBookAir.local>
2024-05-15 11:49:02 +0900

    Update Quiz Game

--- Coffe Machine/main.py
+++ Coffe Machine/main.py
@@ -1,18 +1,32 @@
 import Resources
 
 off = False
+User_input = ""
+remained_resources = Resources.resources
+
 
 def Terminal():
+    global off
+    global User_input
+
     User_input = input("What would you loke? (espresso/latte/cappuccino): ").lower()
     if User_input == "off":
         off = True
-    if User_input == "report":
-        print(Resources.resources)
+    elif User_input == "report":
+        print(f"Water : {remained_resources['water']}ml")
+        print(f"Milk : {remained_resources['milk']}ml")
+        print(f"Coffee : {remained_resources['coffee']}g")
+        print(f"Money : ${remained_resources['water']}")
 
+def drink_select(drink_name):
+    selected_drink = input(Resources.MENU(drink_name))
 
+    
 
 while not off:
     Terminal()
+    print(User_input)
+    print(remained_resources)
+    remained_resources['water'] -= 100
 
-
-    
+print(remained_resources)    
