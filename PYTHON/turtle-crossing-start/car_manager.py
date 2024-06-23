d3538e2b11450387390721fde6aec95ac55caf86
SalmonWave <149510909+SalmonWave@users.noreply.github.com>
2024-06-24 00:16:48 +0900

    Add files via upload

--- PYTHON/turtle-crossing-start/car_manager.py
+++ PYTHON/turtle-crossing-start/car_manager.py
@@ -1,7 +1,21 @@
+from turtle import Turtle
+import random
+
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
 
-class CarManager:
-    pass
+class CarManager(Turtle):
+    
+    def __init__(self):
+        super().__init__()
+        self.shape("square")
+        self.color(COLORS[random.randint(0,5)])
+        self.penup()
+        self.goto(300, random.randint(-250, 250))
+        self.shapesize(1,2,0)
+
+    def move_car(self):
+        new_x = self.xcor() - 4
+        self.goto(new_x, self.ycor())
